871675bf01a21c931f8ed1cf868621fd050bcd04
Oriol Bover Vila <oriolbovervila@Oriols-Mac-mini.local>
2021-10-21 19:42:28 +0200

    add final project course2

--- course2/project/ticky_check.py
+++ course2/project/ticky_check.py
@@ -1,50 +1,77 @@
 #!/usr/bin/env python3
 import re
-import csv
 import operator
 import sys
 
-per_user = {}
-errors = {}
-pattern = r"ticky: ([\w+]*):? ([\w' ]*) [\[[0-9#]*\]?]? ?\((.*)\)$"
 
-logfile = 'syslog.log'
-f = open(logfile, 'r')
+def search_pattern(pattern, line):
+    result = re.search(pattern, line)
+    # egde case
+    if result == None:
+        return None
+    # return only the captured group
+    return result.group(1)
 
-error_file = 'error_message.csv'
-user_file = 'user_statistics.csv'
 
-for log in f:
-    result = re.search(pattern, log)
+# Parse the syslog.log file into two different dictionarys. Use for loops to count each occurence of each regex pattern above
+def parse_log(syslog):
+    # Dictionaries to keep count of each number and user interaction
+    error = {}
+    per_user = {}
+    info_pattern = r"ticky: INFO ([\w ]*)"
+    error_pattern = r"ticky: ERROR ([\w'? ]*)"
+    user_pattern = r"\((.*)\)"
 
-    if result.group(2) not in errors.keys():
-        errors[result.group(2)] = 0
-    errors[result.group(2)] += 1
+    with open(syslog, 'r') as syslog:
+        for line in syslog.readlines():
+            res_err = search_pattern(error_pattern, line)
+            res_info = search_pattern(info_pattern, line)
+            res_user = search_pattern(user_pattern, line)
+            # Check if the name of the user is already in the per_user dictionary
+            if res_user not in per_user:
+                per_user[res_user] = {'INFO': 0, 'ERROR': 0}
+            # Check if the kind of error is already in the error dictionary
+            elif res_err != None and res_err not in error:
+                error[res_err] = 0
+            # If there is an error
+            elif res_err != None:
+                # increment the value of key 'ERROR' by 1 'INFO' in per_user dictionary
+                per_user[res_user]['ERROR'] += 1
+                # increment the type of that error in the error dictionart
+                error[res_err] += 1
+            # If there is an info string only increment the value of key 'INFO' in per_user dictionary
+            elif res_info != None:
+                per_user[res_user]['INFO'] += 1
+        syslog.close()
+    return (error, per_user)
 
-    if result.group(3) not in per_user.keys():
-        per_user[result.group(3)] = {}
-        per_user[result.group(3)]["INFO"] = 0
-        per_user[result.group(3)]["ERROR"] = 0
 
-    if result.group(1) == "INFO":
-        per_user[result.group(3)]["INFO"] += 1
-    elif result.group(1) == "ERROR":
-        per_user[result.group(3)]["ERROR"] += 1
+# Generate report function to sort and write error into a csv report file
 
-errors = sorted(errors.items(), key=operator.itemgetter(1), reverse=True)
-per_user = sorted(per_user.items())
+def generate_error_report(dictionary, report_file):
+    with open(report_file, "w+") as f:
+        f.write("Error"+', ' + "Count" + '\n')
+        for k in sorted(dictionary.items(), key=operator.itemgetter(1), reverse=True):
+            f.write(str(k[0])+', '+str(k[1])+'\n')
+        f.close()
 
-f.close()
-errors.insert(0, ('Error', 'Count'))
 
-f = open(error_file, 'w')
-for error in errors:
-    a, b = error
-    f.write(str(a)+','+str(b)+'\n')
-f.close()
+# Generate report function to sort and write user stats into a csv report file
+def generate_user_report(dictionary, report_file):
+    with open(report_file, "w+") as f:
+        f.write("NAME"+', '+"INFO"+', '+"ERROR" + '\n')
+        for k in sorted(dictionary.items(), key=operator.itemgetter(0)):
+            f.write(
+                str(k[0])+', '+str(k[1]['INFO'])+', '+str(k[1]['ERROR']) + '\n')
+        f.close()
 
-f = open(user_file, 'w')
-f.write('Username, INFO, ERROR\n')
-for stats in per_user:
-    a, b = stats
-    f.write(str(a)+','+str(b["INFO"])+str(b["ERROR"])+'\n')
+
+if __name__ == "__main__":
+    # Calling definitions
+    syslog = sys.argv[1]
+    error, per_user = parse_log(syslog)
+    generate_error_report(
+        error, 'error_message.csv')
+    generate_user_report(
+        per_user, 'user_statistics.csv')
+    sys.exit(0)
